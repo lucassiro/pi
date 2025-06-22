823ad9f80f5a4f8776c299d26cd37f192a5e3eb5
Lucas Siqueira <siqueiraq@gmail.com>
2025-06-22 11:29:07 -0300

    chore: replace sqlmodel to sqlalchemy

--- data_ingestion/services/db_service.py
+++ data_ingestion/services/db_service.py
@@ -1,36 +1,41 @@
-from sqlmodel import Field, Session, SQLModel, create_engine
-from tqdm import tqdm
+from sqlalchemy import Column, Float, Integer, String, create_engine, insert
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from data_ingestion.services.log_service import logger
 
-
-class Despesas(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    nome_deputado: str
-    ano: int
-    mes: int
-    tipo_despesa: str
-    data_documento: str
-    valor_documento: float
-    cnpj_cpf_fornecedor: str
-    valor_liquido: float
-    valor_glosa: float
-    fonte: str
+Base = declarative_base()
 
 
-class Fornecedores(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    nome_fornecedor: str
-    cnpj_cpf_fornecedor: str
-    fonte: str
+class Despesas(Base):
+    __tablename__ = "despesas"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome_deputado = Column(String)
+    ano = Column(Integer)
+    mes = Column(Integer)
+    tipo_despesa = Column(String)
+    data_documento = Column(String)
+    valor_documento = Column(Float)
+    cnpj_cpf_fornecedor = Column(String)
+    valor_liquido = Column(Float)
+    valor_glosa = Column(Float)
+    fonte = Column(String)
 
 
-class Deputados(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    nome: str
-    sigla_partido: str
-    id_legislatura: int
-    sigla_uf: str
+class Fornecedores(Base):
+    __tablename__ = "fornecedores"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome_fornecedor = Column(String)
+    cnpj_cpf_fornecedor = Column(String, unique=True)
+    fonte = Column(String)
+
+
+class Deputados(Base):
+    __tablename__ = "deputados"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String, unique=True)
+    sigla_partido = Column(String)
+    id_legislatura = Column(Integer)
+    sigla_uf = Column(String)
 
 
 class DBService:
@@ -45,35 +50,44 @@ class DBService:
     ) -> None:
         if local:
             self.engine = create_engine("sqlite:///database.db")
+            self.dialect = "sqlite"
         else:
-            self.engine = create_engine(
-                f"mysql+mysqldb://{user}:{password}@{host}[:{port}]/{dbname}", pool_recycle=3600
-            )
+            self.engine = create_engine(f"mysql+mysqldb://{user}:{password}@{host}:{port}/{dbname}", pool_recycle=3600)
+            self.dialect = "mysql"
+        self.Session = sessionmaker(bind=self.engine)
 
     def insert_data(self, deputados: list, despesas: list, fornecedores: list) -> None:
-        SQLModel.metadata.create_all(self.engine)
+        Base.metadata.create_all(self.engine)
+        session = self.Session()
 
-        with Session(self.engine) as session:
+        try:
             logger.info("Inserindo deputados")
-            for dep in tqdm(deputados):
-                if session.get(Deputados, dep["id"]) is not None:
-                    continue
-                dep_tuple = Deputados(**dep)
-                session.add(dep_tuple)
+            if deputados:
+                stmt = insert(Deputados)
+                if self.dialect == "sqlite":
+                    stmt = stmt.prefix_with("OR IGNORE")
+                elif self.dialect == "mysql":
+                    stmt = stmt.prefix_with("IGNORE")
+                session.execute(stmt, deputados)
 
-            logger.info("Inserindo fonecedores")
-            added = []
-            for forn in tqdm(fornecedores):
-                if forn["cnpj_cpf_fornecedor"] in added:
-                    pass
-                else:
-                    forn_tuple = Fornecedores(**forn)
-                    session.add(forn_tuple)
-                    added.append(forn["cnpj_cpf_fornecedor"])
+            logger.info("Inserindo fornecedores")
+            if fornecedores:
+                stmt = insert(Fornecedores)
+                if self.dialect == "sqlite":
+                    stmt = stmt.prefix_with("OR IGNORE")
+                elif self.dialect == "mysql":
+                    stmt = stmt.prefix_with("IGNORE")
+                session.execute(stmt, fornecedores)
 
             logger.info("Inserindo despesas")
-            for desp in tqdm(despesas):
-                desp_tuple = Despesas(**desp)
-                session.add(desp_tuple)
+            if despesas:
+                stmt = insert(Despesas)
+                if self.dialect == "sqlite":
+                    stmt = stmt.prefix_with("OR IGNORE")
+                elif self.dialect == "mysql":
+                    stmt = stmt.prefix_with("IGNORE")
+                session.execute(stmt, despesas)
 
             session.commit()
+        finally:
+            session.close()
